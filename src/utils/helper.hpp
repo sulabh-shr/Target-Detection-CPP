3d2498d4653bdae04fb38aea62bdf721314d01f2
xitizmandal <xitizmandal@gmail.com>
2017-10-04 18:03:52 +0545

    Improve RMSE, transform matrix

--- src/utils/helper.hpp
+++ src/utils/helper.hpp
@@ -10,12 +10,34 @@
 
 #include <opencv2/opencv.hpp>
 
-double rootMeanSquaredError(std::vector<double>, std::vector<double>);
 
+/** @brief: Calculates the Root Mean Square Error (RMSE).
+ *
+ * @param calcualted_values values for the
+ * @param actual_values standard values to compare to.
+ *
+ * @return RMSE of the values.
+ * */
+double rootMeanSquaredError(std::vector<double> calculated_values, std::vector<double> actual_values);
+
+
+/** @brief: Display a window for the input image.
+ *
+ * @param frame input
+ * @param name name of the window
+ * @param pos_x x location for the window position.
+ * @param pos_y y location for the window position.
+ * @param x_size width of the window.
+ * @param y_size height of the window.
+ * */
 void showVideo(cv::Mat frame, const std::string name, int pos_x = -1, int pos_y = -1, int x_size=712, int y_size=400);
 
-void perspectiveTransform(cv::Mat image);
 
+/** @brief: Calculates the transformation matrix.
+ *
+ * @param image input image to use as a reference for viewing the result of transformation.
+ * */
+void perspectiveTrasnsform(cv::Mat image);
 
 
 #endif /* SRC_UTILS_HELPER_HPP_ */
