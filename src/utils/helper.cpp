9e224de440f4f5a04c69cb58e6ef5015beec6cdd
xitizmandal <xitizmandal@gmail.com>
2017-10-04 16:53:14 +0545

    Perspective Transform mockup

--- src/utils/helper.cpp
+++ src/utils/helper.cpp
@@ -25,3 +25,31 @@ void showVideo(cv::Mat frame,const std::string name, int pos_x = -1, int pos_y =
 		cv::moveWindow(name, pos_x, pos_y);
 	cv::imshow(name, frame);
 }
+
+void perspectiveTrasnsform(cv::Mat image){
+	/*
+	 * TODO General process of the perspective Transform. Need to improve.
+	 * */
+	int img_w, img_h, offset = 200;
+
+	img_w = image.cols;
+	img_h = image.rows;
+
+	cv::Point2f source_points[4];
+	cv::Point2f dest_points[4];
+
+	source_points[0] = cv::Point2f(0, 0);
+	source_points[1] = cv::Point2f(img_w, 0);
+	source_points[2] = cv::Point2f(img_w, img_h);
+	source_points[3] = cv::Point2f(0, img_h);
+
+	dest_points[0] = cv::Point2f(0, 0);
+	dest_points[1] = cv::Point2f(img_w, 0);
+	dest_points[2] = cv::Point2f(img_w, img_h+offset);
+	dest_points[3] = cv::Point2f(0, img_h+offset);
+
+	cv::Mat dst;
+	cv::Mat transform_matrix;
+
+	transform_matrix = cv::getPerspectiveTransform(source_points, dest_points);
+}
--- src/utils/helper.hpp
+++ src/utils/helper.hpp
@@ -14,7 +14,7 @@ double rootMeanSquaredError(std::vector<double>, std::vector<double>);
 
 void showVideo(cv::Mat frame, const std::string name, int pos_x = -1, int pos_y = -1, int x_size=712, int y_size=400);
 
-
+void perspectiveTransform(cv::Mat image);
 
 
 
